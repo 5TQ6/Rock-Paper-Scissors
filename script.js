3f9470bc1648a8e7e016eb08ed412fe0060ae00e
5tq6 <hemazoom61@gmail.com>
2022-08-03 17:39:30 +0300

    Second: playing the game with several round and declaring the winner after all

--- script.js
+++ script.js
@@ -4,26 +4,54 @@ function getComputerChoice() {
     return choices[index];
 }
 
-var computerSelection = getComputerChoice();
-var playerSelection = prompt('Choose your weapon:')
-playerSelection = playerSelection.toLowerCase();
-
+var humanScore = 0;
+var computerScore = 0;
 
 function declareWinner(computerSelection,playerSelection) {
         if (computerSelection === playerSelection)
         {
             console.log("Tie")
+            humanScore++;
+            computerScore++;
         }
         else if (computerSelection == "rock" && playerSelection == "scissors" || computerSelection == "paper" && playerSelection == "rock" || computerSelection == "scissors" && playerSelection == "paper" )
         {
-            console.log("computer wins")
+            console.log("computer")
+            console.log(computerSelection, "beats" ,playerSelection)
+            computerScore++;
         }
         else 
         {
-            console.log("Human wins")
+            console.log("Human")
+            console.log(playerSelection, "beats", computerSelection)
+            humanScore++;
         }
 }
 
-console.log(playerSelection)
-console.log(computerSelection)
-declareWinner(computerSelection,playerSelection)
+function game() {
+    for(let i = 0; i < 5; i++)
+    {
+        var computerSelection = getComputerChoice();
+        var playerSelection = prompt('Choose your weapon:')
+        playerSelection = playerSelection.toLowerCase();
+        console.log(playerSelection)
+        console.log(computerSelection)
+        declareWinner(computerSelection,playerSelection)
+        document.write("Human:", humanScore," Computer:", computerScore)
+       rock
+    }
+    if (humanScore == computerScore)
+    {
+        document.write("Tie score")
+    }
+    else if (humanScore > computerScore)
+    {
+        document.write("Human score win")
+    }
+    else 
+    {document.write("Computer score win")}
+}
+
+game()
+
+
